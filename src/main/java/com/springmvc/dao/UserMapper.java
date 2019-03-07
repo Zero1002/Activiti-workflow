77afe31de1dbe5a86ecc888ccb0162b5600535c1
Anna <649314761@qq.com>
2019-03-08 00:17:29 +0800

    框架搭建并引入Activiti引擎

---
+++ src/main/java/com/springmvc/dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.springmvc.dao;
+
+import com.springmvc.pojo.User;
+import com.springmvc.pojo.UserKey;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(UserKey key);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/springmvc/pojo/User.java
@@ -0,0 +1,95 @@
+package com.springmvc.pojo;
+
+import java.util.Date;
+
+public class User extends UserKey {
+    private Integer roleId;
+
+    private Integer gender;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private String avator;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private Boolean isDel;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getAvator() {
+        return avator;
+    }
+
+    public void setAvator(String avator) {
+        this.avator = avator == null ? null : avator.trim();
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Boolean isDel) {
+        this.isDel = isDel;
+    }
+}
